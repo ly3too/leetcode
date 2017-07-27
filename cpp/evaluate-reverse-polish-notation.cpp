d2e48e3d2448e1d8d2c7705ad17e4fbb91edf615
ly3too <ly3too@qq.com>
2017-07-27 10:08:29 +0800

    getRPN

--- cpp/evaluate-reverse-polish-notation.cpp
+++ cpp/evaluate-reverse-polish-notation.cpp
@@ -17,7 +17,11 @@ Some examples:
 #include <ctime>
 #include <ctype.h>
 #include <sstream>
-//#include "print_container.hpp"
+#include <map>
+#include <cmath>
+#include <unordered_map>
+#include <typeinfo>
+#include "print_container.hpp"
 
 using namespace std;
 
@@ -55,6 +59,12 @@ public:
                 case '/':
                     values.push(x/y);
                     break;
+                case '%':
+                    values.push(x%y);
+                    break;
+                case '^':
+                    values.push(static_cast<int>(pow(x,y)));
+                    break;
                 default:
                     break;
                 }
@@ -63,8 +73,68 @@ public:
 
         return values.empty() ? 0 : values.top();
     }
-};
 
+    vector<string> getRPN(const string &s) {
+        vector<string> tokens;
+        stringstream ss(s);
+        string tmp;
+        while(getline(ss, tmp, ' ')) {
+            tokens.emplace_back(move(tmp));
+        }
+
+        const char L = 'L';
+        const char R = 'R';
+        unordered_map<char, pair<int,char>> operators{{'+',{1,L}},
+                                {'-', {1,L}},
+                                {'*', {2,L}},
+                                {'/', {2,L}},
+                                {'%', {3,L}},
+                                {'^', {3,R}},
+                                {'(', {0,L}},};
+
+        vector<string> output;
+        stack<string> ops;
+        for (auto &token : tokens) {
+            if (token.empty())
+                continue;
+
+            if (token.size()>1 || isdigit(token[0])) {
+                output.emplace_back(move(token));
+
+            } else if(token[0] == '(') {
+                ops.push(token);
+
+            } else if(token[0] == ')') {
+                while((!ops.empty()) && (ops.top()[0] != '(')) {
+                    output.emplace_back(move(ops.top()));
+                    ops.pop();
+                }
+                if (!ops.empty()) {
+                    ops.pop();
+                }
+
+                /*higher priority*/
+            } else if (ops.empty() ||
+                    operators[token[0]].second==L && operators[token[0]].first > operators[ops.top()[0]].first ||
+                    operators[token[0]].second==R && operators[token[0]].first >= operators[ops.top()[0]].first) {
+                        ops.push(move(token));
+
+                /*lower priority*/
+            } else {
+                output.emplace_back(move(ops.top()));
+                ops.pop();
+                ops.push(token);
+            }
+        }
+
+        while (!ops.empty()) {
+            output.emplace_back(move(ops.top()));
+            ops.pop();
+        }
+
+        return output;
+    }
+};
 
 
 int main() {
@@ -80,8 +150,18 @@ int main() {
         vvs.emplace_back(move(vs_tmp));
     }
 
-    //cout << string{"hello"} << endl;
+    cout << initializer_list<int>{0,1,2} << endl;
+    cout << string{"hello"} << endl;
+    cout << pair<int,int>{0,1} << endl;
+    cout << map<int,int>{{2,2},{3,4}} << endl;
+    cout << vvs << endl;
     for (auto& vs : vvs) {
-        cout << " -> " << Solution{}.evalRPN(vs) << endl;
+        cout << vs << " -> " << Solution{}.evalRPN(vs) << endl;
+    }
+
+    in_str = {"", "1 + 2", "1 + 2 - 2 ^ 3", "4 + 13 % 5", "5 + ( 6 + 8 ) * 2"};
+    for (auto &s : in_str) {
+        auto&& sv = Solution{}.getRPN(s);
+        cout << s << " -> " << sv << " = " << Solution{}.evalRPN(sv) << endl;
     }
 }
--- cpp/print_container.hpp
+++ cpp/print_container.hpp
@@ -2,6 +2,7 @@
 #include <utility>
 #include <string>
 
+/*
 template<typename T, typename... Args, template<typename, typename...> class Tclass>
 std::ostream& operator<<(std::ostream &out, const Tclass<T, Args...> &c) {
     out << "{";
@@ -15,8 +16,84 @@ std::ostream& operator<<(std::ostream &out, const Tclass<T, Args...> &c) {
     return out;
 }
 
+template<>
+std::ostream& operator<<(std::ostream &out, const std::string &c) {
+    out << "{";
+    for (auto p=c.begin(), end=c.end(); p != end; ++p) {
+        out << *p;
+        if (p+1 != end) {
+            out << ", ";
+        }
+    }
+    out << "}";
+    return out;
+}
+*/
+/*
+template<typename T, typename Args, template<typename, typename> class Tclass>
+std::ostream& operator<<(std::ostream &out, const Tclass<T, Args> &c) {
+    out << "{";
+    for (auto p=c.begin(), end=c.end(); p != end;) {
+        out << *p;
+        ++p;
+        if (p != end) {
+            out << ", ";
+        }
+    }
+    out << "}";
+    return out;
+}
+*/
+
 template<typename T, typename U>
 std::ostream& operator<<(std::ostream &out, const std::pair<T, U> &p) {
     out << '[' << p.first << ", " << p.second << "]";
     return out;
 }
+
+/*
+template<typename T, typename U>
+std::ostream& operator<<(std::ostream &out, const std::map<T, U> &c) {
+    out << "{";
+    for (auto p=c.begin(), end=c.end(); p != end;) {
+        out << *p;
+        ++p;
+        if (p != end) {
+            out << ", ";
+        }
+    }
+    out << "}";
+    return out;
+}
+*/
+
+template <class charT, class traits, class T, class... Args, template<typename, typename...> class Tclass>
+  std::basic_ostream<charT,traits>& operator<< (std::basic_ostream<charT,traits>& out, const Tclass<T, Args...>& c) {
+      out << "{";
+      for (auto p=c.begin(), end=c.end(); p != end;) {
+          out << *p;
+          ++p;
+          if (p != end) {
+              out << ", ";
+          }
+      }
+      out << "}";
+      return out;
+}
+
+/*
+template<typename T>
+std::ostream& print(std::ostream &out, T const &val) {
+  return (out << val << " ");
+}
+
+template<typename T1, typename T2>
+std::ostream& print(std::ostream &out, std::pair<T1, T2> const &val) {
+  return (out << "{" << val.first << " " << val.second << "} ");
+}
+
+template<template<typename, typename...> class TT, typename... Args>
+std::ostream& operator<<(std::ostream &out, TT<Args...> const &cont) {
+  for(auto&& elem : cont) print(out, elem);
+  return out;
+}*/

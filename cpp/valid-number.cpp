3e9f695439b2296c976092e234ddf72494d5b288
ly3too <ly3too@qq.com>
2017-08-19 11:22:10 +0800

    valid-number state matchine

--- cpp/valid-number.cpp
+++ cpp/valid-number.cpp
@@ -9,10 +9,78 @@ Some examples:
 "2e10" => true
 */
 
+/* regular expression very slow */
 class Solution {
 public:
     bool isNumber(string s) {
         regex e(R"(^\s*[\+\-]?((\d+(\.\d*)?)|\.\d+)([eE][\+\-]?\d+)?\s*$)");
         return regex_match(s, e);
+    }
+};
+
+/* state machine O(n) */
+class Solution {
+public:
+    bool isNumber(string s) {
+        enum State:int {
+            invalid = 0,
+            space, //1 soace before number
+            sign, //2
+            digit, //3
+            dot, //4
+            expon //5
+        };
+        /*
+        state :
+        -1 : invalid
+        0 : pre_space
+        1 : suf_space
+        2 : base_sign
+        3 : base_int
+        4 : pure_dot
+        5 : base_frac
+        6 : e_sign
+        7 : exp
+        8 : e_digit
+        */
+        const vector<vector<int>> state_table {
+            {-1, 0, 2, 3, 4, -1}, //state 0, for initial and prespace
+            {-1, 1, -1, -1, -1, -1}, //state 1, for suf_space
+            {-1, -1, -1, 3, 4, -1}, //state 2, for base_sign
+            {-1, 1, -1, 3, 5, 7}, //state 3, for base_int
+            {-1, -1, -1, 5, -1, -1}, //state 4, for pure_dot
+            {-1, 1, -1, 5, -1, 7}, //state 5, for base_frac
+            {-1, -1, -1, 8, -1, -1}, //state 6, for e_sign
+            {-1, -1, 6, 8, -1, -1}, //state 7, for exp
+            {-1, 1, -1, 8, -1, -1} //state 8, for e_digit
+        };
+
+        int state = 0;
+        for (auto &ch : s) {
+            int input = State::invalid;
+            if (ch == ' ') {
+                input = State::space;
+            } else if (ch == '-' || ch == '+') {
+                input = State::sign;
+            } else if (ch >= '0' && ch <= '9') {
+                input = State::digit;
+            } else if (ch == '.') {
+                input = State::dot;
+            } else if (ch == 'e' || ch == 'E') {
+                input = State::expon;
+            }
+
+            state = state_table[state][input];
+            if (state == -1) {
+                return false;
+            }
+        }
+
+        if (state == 1 || state == 3 || state == 5 || state == 8) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 };

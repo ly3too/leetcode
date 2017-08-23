3d16d46700381ecf74af228410203d8f83dac1dd
ly3too <ly3too@qq.com>
2017-08-23 22:36:24 +0800

    pow improve

--- cpp/powx-n.cpp
+++ cpp/powx-n.cpp
@@ -27,3 +27,29 @@ public:
         }
     }
 };
+
+/* O(logN) */
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (x == 0) {
+            if (n == 0 || n==-1)
+                throw overflow_error("math error");
+            return 0;
+        }
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        if (n == -1)
+            return 1/x;
+
+        double tmp = myPow(x, n >> 1);
+        tmp *= tmp;
+        if (n%2 == 0) {
+            return tmp;
+        } else {
+            return x * tmp;
+        }
+    }
+};

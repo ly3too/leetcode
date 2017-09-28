003708ccc6f31b8deb360d38fbf1c6865d11bfe7
ly3too <ly3too@qq.com>
2017-09-28 22:13:35 +0800

    merge_sort

---
+++ cpp/merge_sort.cpp
@@ -0,0 +1,55 @@
+#include "header.hpp"
+
+using namespace std;
+
+template <class Valtype, class Comp = less<Valtype>>
+void merge_sort(vector<Valtype> &dest, vector<Valtype> &src, int beg, int end, Comp comp = Comp()) {
+    if (beg == end) {
+        dest[beg] = src[beg];
+        return;
+    }
+    int mid = (beg+end) / 2;
+    merge_sort(src, dest, beg, mid, comp);
+    merge_sort(src, dest, mid+1, end, comp);
+
+    int i = beg;
+    int j = mid+1;
+    int k = beg;
+    while (i <= mid && j <= end) {
+        if (comp(src[i], src[j])) {
+            dest[k++] = src[i++];
+        } else {
+            dest[k++] = src[j++];
+        }
+    }
+
+    while (i <= mid) {
+        dest[k++] = src[i++];
+    }
+
+    while (j <= end) {
+        dest[k++] = src[j++];
+    }
+}
+
+template <class Valtype, class Comp = less<Valtype>>
+void merge_sort(vector<Valtype> &vet, Comp comp = Comp()) {
+    if (vet.size() <= 1)
+        return;
+
+    auto cp = vet;
+    merge_sort(vet, cp, 0, vet.size()-1, comp);
+}
+
+int main() {
+    vector<int> input;
+    for (int i=0; i < 10; ++i) {
+        input.emplace_back(rand()%100);
+    }
+
+    cout << input << endl;
+
+    merge_sort(input, less<int>());
+
+    cout << input << endl;
+}

0dcb68781add0c1f66b47c479aa0feb6ce40b44b
ly3too <ly3too@qq.com>
2017-10-05 20:12:04 +0800

    reconstruct queue by height

--- cpp/queue-reconstruction-by-height.cpp
+++ cpp/queue-reconstruction-by-height.cpp
@@ -30,3 +30,25 @@ public:
         return res;
     }
 };
+
+/* sort by height in descending order, by number of people infront in ascending order, then insert one by one , O(n^2), O(1)*/
+class Solution {
+public:
+    vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
+        auto cmp = [](pair<int, int> &a, pair<int, int> &b) {
+            return a.first == b.first ? a.second < b.second : b.first < a.first;};
+
+        sort(people.begin(), people.end(), cmp);
+
+        auto N = people.size();
+        for (int i=0; i<N; ++i) {
+            auto tmp = people[i];
+            for (int j=i; j>tmp.second; --j) {
+                people[j] = people[j-1];
+            }
+            people[tmp.second] = tmp;
+        }
+
+        return people;
+    }
+};

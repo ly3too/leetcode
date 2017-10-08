bf1913b18e8a001cfb334aafccedd69494ba80ef
ly3too <ly3too@qq.com>
2017-10-08 16:47:32 +0800

    populating next right pointers

--- cpp/flatten-binary-tree-to-linked-list.cpp
+++ cpp/flatten-binary-tree-to-linked-list.cpp
@@ -24,3 +24,27 @@ public:
 private:
     TreeNode* new_root = nullptr;
 };
+
+/* double linklist */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root)
+            return;
+
+        flatten(root -> right);
+        flatten(root -> left);
+        root -> right = new_root;
+
+        root -> left = nullptr;
+        if (root -> right) {
+            root -> right -> left = root;
+        }
+
+        new_root = root;
+
+    }
+
+private:
+    TreeNode* new_root = nullptr;
+};
---
+++ cpp/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,58 @@
+/*
+Given a binary tree
+
+    struct TreeLinkNode {
+      TreeLinkNode *left;
+      TreeLinkNode *right;
+      TreeLinkNode *next;
+    }
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+For example,
+Given the following perfect binary tree,
+         1
+       /  \
+      2    3
+     / \  / \
+    4  5  6  7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \  / \
+    4->5->6->7 -> NULL
+*/
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root)
+            return;
+            
+        root -> next = nullptr;
+
+        while (root) {
+            auto cur = root;
+            while (cur) {
+                if (cur -> left)
+                    cur -> left -> next = cur -> right;
+                if (cur -> right && cur -> next) {
+                    cur -> right -> next = cur -> next -> left;
+                }
+
+                cur = cur -> next;
+            }
+
+            root = root -> left;
+        }
+    }
+};

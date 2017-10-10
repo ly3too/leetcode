c55414ac46ec48f5d00140e5db355f882e5fbc1a
ly3too <ly3too@qq.com>
2017-10-10 15:56:52 +0800

    median of two sorted array

--- cpp/median-of-two-sorted-arrays.cpp
+++ cpp/median-of-two-sorted-arrays.cpp
@@ -37,7 +37,7 @@ private:
         while (start < end) {
             auto mid = (start + end + 1) / 2;
 
-            if ((k-mid >= 0 && k-mid < n && nums2[k-mid] < nums1[mid]) || mid > k) {
+            if ((k-mid >= 0 && k-mid < n && nums2[k-mid] < nums1[mid]) || mid > k || k-mid >= n) { // nums1[mid] is bigger than kth number
                 end = mid-1;
             } else {
                 start = mid;
@@ -50,5 +50,50 @@ private:
             return max(nums2[k-end-1], nums1[end]);
         else
             return nums1[end];
+    }
+};
+
+// Time:  O(log(min(m, n)))
+// Space: O(1)
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if ((nums1.size() + nums2.size()) % 2 == 1) {
+            return findKthInTwoSortedArrays(nums1, nums2, (nums1.size() + nums2.size()) / 2 + 1);
+        } else {
+            return (findKthInTwoSortedArrays(nums1, nums2, (nums1.size() + nums2.size()) / 2) +
+                    findKthInTwoSortedArrays(nums1, nums2, (nums1.size() + nums2.size()) / 2 + 1)) / 2.0;
+        }
+    }
+
+    int findKthInTwoSortedArrays(const vector<int>& A, const vector<int>& B,
+                                 int k) {
+        const int m = A.size();
+        const int n = B.size();
+
+        // Make sure m is the smaller one.
+        if (m > n) {
+            return findKthInTwoSortedArrays(B, A, k);
+        }
+
+        int left = 0;
+        int right = m;
+        // Find a partition of A and B
+        // where min left s.t. A[left] >= B[k - 1 - left]. Thus left is the (k + 1)-th element.
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (0 <= k - 1 - mid && k - 1 - mid < n && A[mid] >= B[k - 1 - mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        int Ai_minus_1 = left - 1 >= 0 ? A[left - 1] : numeric_limits<int>::min();
+        int Bj = k - 1 - left >= 0 ? B[k - 1 - left] : numeric_limits<int>::min();
+
+        // kth element would be A[left - 1] or B[k - 1 - left].
+        return max(Ai_minus_1, Bj);
     }
 };

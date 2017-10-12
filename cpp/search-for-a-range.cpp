97c8465d2d22feb20d28592a54cdaf9ea912b894
ly3too <ly3too@qq.com>
2017-10-12 19:05:40 +0800

    search in rotated sorted array

---
+++ cpp/search-for-a-range.cpp
@@ -0,0 +1,67 @@
+/*
+Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].
+*/
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto begin = lower_bound(nums.cbegin(), nums.cend(), target);
+        auto end = upper_bound(nums.cbegin(), nums.cend(), target);
+        if (begin != end && *begin == target)
+          return {begin - nums.cbegin(), end - nums.cbegin() - 1};
+
+        return {-1, -1};
+    }
+};
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+
+        auto begin = lower_bound(nums, target);
+        auto end = upper_bound(nums, target);
+        if (begin != end && nums[begin] == target)
+          return {begin, end - 1};
+
+        return {-1, -1};
+    }
+    
+  private:
+    int lower_bound(vector<int>& nums, int target) {
+      auto s = 0;
+      auto e = static_cast<int>(nums.size()) - 1;
+      while (s < e) {
+        auto mid = (s + e) / 2;
+        if (nums[mid] < target) {
+          s = mid + 1;
+        } else {
+          e = mid;
+        }
+      }
+
+      return s;
+    }
+
+    int upper_bound(vector<int>& nums, int target) {
+      auto s = 0;
+      auto e = static_cast<int>(nums.size()) - 1;
+      while (s < e) {
+        auto mid = (s + e) / 2;
+        if (nums[mid] <= target) {
+          s = mid + 1;
+        } else {
+          e = mid;
+        }
+      }
+
+      return s;
+    }
+};
---
+++ cpp/search-insert-position.cpp
@@ -0,0 +1,29 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        auto s = 0;
+        auto e = static_cast<int>(nums.size());
+        while (s < e) {
+          auto mid = (s + e - 1) / 2;
+          if (nums[mid] < target) {
+            s = mid + 1;
+          } else {
+            e = mid;
+          }
+        }
+        return s;
+    }
+};

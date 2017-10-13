ddaa62aa953e5a2baf71cfadec73fa2a7c7cf52e
ly3too <ly3too@qq.com>
2017-10-13 11:17:28 +0800

    count complete BST nodes

--- cpp/count-complete-tree-nodes.cpp
+++ cpp/count-complete-tree-nodes.cpp
@@ -39,3 +39,52 @@ public:
         return 1 + countNodes(root -> left) + countNodes(root -> right);
     }
 };
+
+/* O(logn^2) */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        auto left = root;
+        auto level = 0;
+        while (left -> left) {
+            left = left -> left;
+            ++level;
+        }
+
+        auto res = pow(2, level) - 1;
+        auto s = 0;
+        auto e = res;
+        while (s <= e) {
+            auto mid = (s + e) / 2;
+            if (exist(root, level, mid)) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+
+        return res + s;
+    }
+
+private:
+    bool exist(TreeNode* root, int level, int cnt) {
+
+        auto half_cnt = static_cast<int>(pow(2, level)) / 2;
+        while (half_cnt && root) {
+            if (cnt >= half_cnt) {
+                root = root -> right;
+                cnt -= half_cnt;
+
+            } else {
+                root = root -> left;
+            }
+
+            half_cnt >>= 1;
+        }
+
+        return root != nullptr;
+    }
+};

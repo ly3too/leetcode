4db7314c79c9bf041c65240f1c0f3815113b56cb
ly3too <ly3too@qq.com>
2017-10-14 22:43:42 +0800

    longest increasing subsequence

---
+++ cpp/russian-doll-envelopes.cpp
@@ -0,0 +1,33 @@
+/*
+You have a number of envelopes with widths and heights given as a pair of integers (w, h).
+One envelope can fit into another if and only if both the width and height of one envelope
+is greater than the width and height of the other envelope.
+
+*/
+
+class Solution {
+public:
+    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        using pr_t = pair<int, int>;
+
+        auto cmp = [] (pr_t &p1, pr_t &p2) {
+            return p1.first == p2.first ? p1.second < p2.second : p1.first < p2.first;};
+
+        sort(envelopes.begin(), envelopes.end(), cmp);
+
+        vector<pr_t> res;
+        for (auto &enve : envelopes) {
+            auto target = enve.second;
+
+            auto it = lower_bound(res.begin(), res.end(), target);
+
+            if (it == res.end()) {
+                res.emplace_back(target);
+            } else {
+                *it = target;
+            }
+        }
+
+        return res.size();
+    }
+};
---
+++ cpp/valid-perfect-square.cpp
@@ -0,0 +1,32 @@
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Returns: True
+*/
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        auto left = 1;
+        auto right = num;
+
+        while (left <= right) {
+            auto mid = (left + right) / 2;
+
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left * left == num;
+    }
+};

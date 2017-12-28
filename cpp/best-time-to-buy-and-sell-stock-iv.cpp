5452d31f6be991c54a11cbd644971292fe59f3b7
ly3too <ly3too@qq.com>
2017-12-28 11:02:51 +0800

    best time to buy and sell stocks

--- cpp/best-time-to-buy-and-sell-stock-iv.cpp
+++ cpp/best-time-to-buy-and-sell-stock-iv.cpp
@@ -34,3 +34,38 @@ public:
         return sell[k-1];
     }
 };
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if (k <= 0 || prices.size() == 0)
+            return 0;
+        if (k >= static_cast<int>(prices.size()) / 2)
+            return quick_solve(prices);
+
+        vector<int> sell(k, 0);
+        vector<int> hold(k, numeric_limits<int>::min());
+
+        for (auto price : prices) {
+            for (int i=k-1; i > 0; --i) {
+                sell[i] = max(sell[i], hold[i] + price);
+                hold[i] = max(hold[i], sell[i-1] - price);
+            }
+            sell[0] = max(sell[0], hold[0] + price);
+            hold[0] = max(hold[0], -price);
+        }
+
+        return sell[k-1];
+    }
+private:
+    int quick_solve(vector<int>& prices) {
+        int ret = 0;
+        auto last = prices[0];
+        for (auto price : prices) {
+            if (price > last)
+                ret += price - last;
+            last = price;
+        }
+        return ret;
+    }
+};
